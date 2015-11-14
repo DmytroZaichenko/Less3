5784b433fc248301c2885a0dcae483fcca63d476
Dmytro Zaichenko <dim2002@ukr.net>
2015-11-14 22:38:46 +0200

    NPE

--- src/constructors/Glyph.java
+++ src/constructors/Glyph.java
@@ -6,7 +6,6 @@ public class Glyph {
     public Glyph() {
         System.out.println("Glyph#constructor");
         draw();
-        throw null;
     }
 
     public void draw(){
--- src/constructors/RoundGlyph.java
+++ src/constructors/RoundGlyph.java
@@ -4,6 +4,7 @@ package constructors;
 public class RoundGlyph extends Glyph {
 
     private int radius = 5;
+    private String s = "new value";
 
     public RoundGlyph(int radius) {
         System.out.println("RoundGlyph#constructor");
@@ -13,6 +14,7 @@ public class RoundGlyph extends Glyph {
 
     @Override
     public void draw() {
+        s.length();
         System.out.println("RoundGlyph#draw()");
         System.out.println("RoundGlyph [radius: " + radius + "]");
     }
